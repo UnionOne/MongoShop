9c5d48d484722b2011b5bad528a4ae7abee54526
Union One <hax2033@gmail.com>
2016-02-25 00:37:40 +0300

    Bootstrap added

---
+++ src/main/java/com/itibo/mongo/bean/FilterView.java
@@ -0,0 +1,16 @@
+package com.itibo.mongo.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+
+/**
+ * Created by union on 22.02.2016.
+ */
+
+@ManagedBean(name = "filter")
+@ViewScoped
+public class FilterView implements Serializable {
+
+
+}
